cb6cc3c6b9187ad11317f0b1da6e06f5ff1cde2c
Jefferson Ismael <144837101+jeffersonismael@users.noreply.github.com>
2025-09-02 22:47:08 +0000

    atribuindo variaveis

--- Introdução à programação de computadores/variaveis.c
+++ Introdução à programação de computadores/variaveis.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    int idade = 33;
+    int idade;
     int quantidade = 1;
     float altura = 1.60;
     double peso = 70.5;
     char letra = 'J';
     char nome[20] = "Jefferson";
+
+    idade = 33;
+    quantidade = 10;
 }
