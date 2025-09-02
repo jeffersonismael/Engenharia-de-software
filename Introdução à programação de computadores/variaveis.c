07d00bdbe8fe6b80ab652ab830790b1a8085113c
Jefferson Ismael <144837101+jeffersonismael@users.noreply.github.com>
2025-09-02 22:41:30 +0000

    variaveis

---
+++ Introdução à programação de computadores/variaveis.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 33;
+    int quantidade = 1;
+    float altura = 1.60;
+    double peso = 70.5;
+    char letra = 'J';
+    char nome[20] = "Jefferson";
+}
